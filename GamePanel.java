807c9c1c2147730dfd83225e96866c786a48bda8
bpdm2710-alt <bpdm2710@gmail.com>
2026-04-30 21:24:02 +0700

    create gameloop mechanism

--- GamePanel.java
+++ GamePanel.java
@@ -1,17 +1,48 @@
 import java.awt.Color;
 import java.awt.Dimension;
-
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
 
+
+    }
+    public void lauchGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    private void update(){
         
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void run() {
+        double DRAW_INTERVAL = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / DRAW_INTERVAL;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
     }
 }
