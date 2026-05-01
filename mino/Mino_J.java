f2560ea250004b53f839234d17aaf0f2c1817054
ChieNg00 <manhchiennguyen007@gmail.com>
2026-05-02 00:07:46 +0700

    Update

--- mino/Mino_J.java
+++ mino/Mino_J.java
@@ -33,7 +33,7 @@ public class Mino_J extends Mino {
         tempB[3].x = b[0].x - Block.SIZE;
         tempB[3].y = b[0].y + Block.SIZE;
 
-        UpdateXY(1);
+        updateXY(1);
     }
     public void getDirection2() {
         // o
@@ -47,7 +47,7 @@ public class Mino_J extends Mino {
         tempB[3].x = b[0].x - Block.SIZE;
         tempB[3].y = b[0].y - Block.SIZE;
 
-        UpdateXY(2);
+        updateXY(2);
     }
     public void getDirection3() {
         // o o
@@ -62,7 +62,7 @@ public class Mino_J extends Mino {
         tempB[3].x = b[0].x + Block.SIZE;
         tempB[3].y = b[0].y - Block.SIZE;
 
-        UpdateXY(3);
+        updateXY(3);
     }
     public void getDirection4() {
         // o o o
@@ -76,6 +76,6 @@ public class Mino_J extends Mino {
         tempB[3].x = b[0].x + Block.SIZE;
         tempB[3].y = b[0].y + Block.SIZE;
 
-        UpdateXY(4);
+        updateXY(4);
     }
 }
--- mino/Mino_O.java
+++ mino/Mino_O.java
@@ -11,11 +11,15 @@ public class Mino_O extends Mino {
     public void setXY(int x, int y){
         b[0].x = x;
         b[0].y = y;
-        b[1].x = b[0].x + Block.SIZE;
-        b[1].y = b[0].y;
-        b[2].x = b[0].x;
-        b[2].y = b[0].y + Block.SIZE;
+        b[1].x = b[0].x;
+        b[1].y = b[0].y + Block.SIZE;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
         b[3].x = b[0].x + Block.SIZE;
         b[3].y = b[0].y + Block.SIZE;
     }
+    public void getDirection1 () {}
+    public void getDirection2 () {}
+    public void getDirection3 () {}
+    public void getDirection4 () {}
 }
