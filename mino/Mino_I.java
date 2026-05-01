c8c85130952d538d475b7b92ec4a455c60457565
ChieNg00 <manhchiennguyen007@gmail.com>
2026-05-02 00:02:51 +0700

    Rotate Mino_I

--- mino/Mino_I.java
+++ mino/Mino_I.java
@@ -2,22 +2,51 @@ package mino;
 import java.awt.Color;
 
 public class Mino_I extends Mino {
-    // o
-    // o 
-    // o
-    // o
+    // 
+    // o o o o
+    // 
     public Mino_I(){
         create(Color.cyan);
     }
     public void setXY(int x, int y){
         b[0].x = x;
         b[0].y = y;
-        b[1].x = b[0].x;
-        b[1].y = b[0].y + Block.SIZE;
-        b[2].x = b[0].x;
-        b[2].y = b[0].y + 2 * Block.SIZE;
-        b[3].x = b[0].x;
-        b[3].y = b[0].y + 3 * Block.SIZE;
+        b[1].x = b[0].x - Block.SIZE;
+        b[1].y = b[0].y;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
+        b[3].x = b[0].x + Block.SIZE * 2;
+        b[3].y = b[0].y;
     }
+    public void getDirection1 () {
+        // 
+        // o o o o
+        // 
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE * 2;
+        tempB[3].y = b[0].y;
 
+        UpdateXY(1);
+    }
+    public void getDirection2 () {
+        // o
+        // o
+        // o
+        // o
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x;
+        tempB[3].y = b[0].y + Block.SIZE * 2;
+        
+        UpdateXY(2);
+    }
 }
