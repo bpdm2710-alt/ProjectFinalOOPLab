487ad1ca9778efb336e60e7c1114e15d775d4713
bpdm2710-alt <bpdm2710@gmail.com>
2026-05-02 00:50:37 +0700

    complete the code for the rotation check

--- mino/Mino.java
+++ mino/Mino.java
@@ -29,22 +29,41 @@ public class Mino {
     }
     public void setXY(int x, int y){}
     public void updateXY(int direction){
-        this.direction = direction;
-        b[0].x = tempB[0].x;
-        b[0].y = tempB[0].y;
-        b[1].x = tempB[1].x;
-        b[1].y = tempB[1].y;
-        b[2].x = tempB[2].x;
-        b[2].y = tempB[2].y;
-        b[3].x = tempB[3].x;
-        b[3].y = tempB[3].y;
+        checkRotationCollision();
+        if (!leftCollision && !rightCollision && !downCollision){
+            this.direction = direction;
+            b[0].x = tempB[0].x;
+            b[0].y = tempB[0].y;
+            b[1].x = tempB[1].x;
+            b[1].y = tempB[1].y;
+            b[2].x = tempB[2].x;
+            b[2].y = tempB[2].y;
+            b[3].x = tempB[3].x;
+            b[3].y = tempB[3].y;
+        }
     }
     public void getDirection1 () {}
     public void getDirection2 () {}
     public void getDirection3 () {}
     public void getDirection4 () {}
+    public void checkRotationCollision () {
+         for (int i = 0; i < b.length; i++){
+            if (tempB[i].x < GameManager.left_x){
+                leftCollision = true;
+            }
+        }
+        for (int i = 0; i < b.length; i++){
+            if (tempB[i].x + Block.SIZE > GameManager.right_x){
+                rightCollision = true;
+            }
+        }
+        for (int i = 0; i < b.length; i++){
+            if (tempB[i].y + Block.SIZE > GameManager.bottom_y){
+                downCollision = true;
+            }
+        }
+    }
     public void checkMovementCollision () {
-
         for (int i = 0; i < b.length; i++){
             if (b[i].x == GameManager.left_x){
                 leftCollision = true;
