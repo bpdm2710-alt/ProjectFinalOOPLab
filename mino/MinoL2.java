e4be4f130aa96e13eba72f5c026bf211d49c38c2
ChieNg00 <manhchiennguyen007@gmail.com>
2026-05-01 23:29:23 +0700

    Rotation of MinoL2.java

--- mino/MinoL2.java
+++ mino/MinoL2.java
@@ -20,4 +20,62 @@ public class MinoL2 extends Mino {
         b[3].x = b[0].x - Block.SIZE;
         b[3].y = b[0].y + Block.SIZE;
     }
+    public void getDirection1() {
+        //   o
+        //   o
+        // o o
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        UpdateXY(1);
+    }
+    public void getDirection2() {
+        // o
+        // o o o
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x + Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x - Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        UpdateXY(2);
+    }
+    public void getDirection3() {
+        // o o
+        // o
+        // o
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        UpdateXY(3);
+    }
+    public void getDirection4() {
+        // o o o
+        //     o
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        UpdateXY(4);
+    }
 }
