533594bfd0dd12e7bba150a9170455e2ebea3f64
ChieNg00 <manhchiennguyen007@gmail.com>
2026-05-02 09:06:23 +0700

    Update rotation

--- mino/Mino_L.java
+++ mino/Mino_L.java
@@ -12,24 +12,24 @@ public class Mino_L extends Mino {
         // o o o
         b[0].x = x;
         b[0].y = y;
-        b[1].x = b[0].x;
-        b[1].y = b[0].y - Block.SIZE;
-        b[2].x = b[0].x;
-        b[2].y = b[0].y + Block.SIZE;
+        b[1].x = b[0].x - Block.SIZE;
+        b[1].y = b[0].y;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
         b[3].x = b[0].x + Block.SIZE;
-        b[3].y = b[0].y + Block.SIZE;
+        b[3].y = b[0].y - Block.SIZE;
     }
     public void getDirection1 () {
         //     o
         // o o o
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x;
-        tempB[1].y = b[0].y - Block.SIZE;
-        tempB[2].x = b[0].x;
-        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
         tempB[3].x = b[0].x + Block.SIZE;
-        tempB[3].y = b[0].y + Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
 
         updateXY(1);
     }
@@ -44,7 +44,7 @@ public class Mino_L extends Mino {
         tempB[2].x = b[0].x + Block.SIZE;
         tempB[2].y = b[0].y;
         tempB[3].x = b[0].x + Block.SIZE;
-        tempB[3].y = b[0].y - Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
 
         updateXY(2);
     }
@@ -53,9 +53,9 @@ public class Mino_L extends Mino {
         // o
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].x = b[0].x + Block.SIZE;
         tempB[1].y = b[0].y;
-        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].x = b[0].x - Block.SIZE;
         tempB[2].y = b[0].y;
         tempB[3].x = b[0].x - Block.SIZE;
         tempB[3].y = b[0].y + Block.SIZE;
@@ -68,12 +68,12 @@ public class Mino_L extends Mino {
         //   o
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x - Block.SIZE;
-        tempB[1].y = b[0].y;
-        tempB[2].x = b[0].x + Block.SIZE;
-        tempB[2].y = b[0].y;
-        tempB[3].x = b[0].x + Block.SIZE;
-        tempB[3].y = b[0].y + Block.SIZE;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
 
         updateXY(4);
     }
