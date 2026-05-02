39dee5742d1d53aa70ea0cf93b9ef82cedfd7660
ChieNg00 <manhchiennguyen007@gmail.com>
2026-05-02 09:12:48 +0700

    Update rotation of Mino_J

--- mino/Mino_J.java
+++ mino/Mino_J.java
@@ -40,10 +40,10 @@ public class Mino_J extends Mino {
         // o
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x + Block.SIZE;
-        tempB[1].y = b[0].y;
-        tempB[2].x = b[0].x - Block.SIZE;
-        tempB[2].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
         tempB[3].x = b[0].x + Block.SIZE;
         tempB[3].y = b[0].y - Block.SIZE;
 
